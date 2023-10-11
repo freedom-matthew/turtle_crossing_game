b87e77ac92e35dca6404af78d9c7a3b680f63e52
Matěj Svoboda <svobodamatej97@gmail.com>
2023-10-11 17:24:33 +0200

    Allowed player to move with turtle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -7,6 +8,11 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+player = Player()
+
+screen.listen()
+screen.onkey(player.move, "Up")
 
 game_is_on = True
 while game_is_on:
